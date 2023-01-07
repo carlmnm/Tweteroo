bacc91987e0f69b2b56f1c56855d3ff721b8a689
carlmnm <carlossmiguell@gmail.com>
2023-01-07 17:33:44 -0300

    saving user data

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 import express from "express"
 import cors from "cors"
 
-const app = express
+const app = express()
 const PORT = 5000
+app.use(express.json())
+app.use(cors())
 
-app.listen(PORT)
+const users = []
+
+{/*app.get('/', (req, res) => {
+    res.send('Hello World')
+})*/}
+
+app.post('/sign-up', (req, res) => {
+    const userData = req.body
+    
+    users.push(userData)
+    res.status(201).send("OK")
+
+})
+
+app.get('/sign-up', (req, res) => {
+    res.send(users)
+})
+
+app.listen(PORT, () => console.log(`Este servidor roda na porta: ${PORT}`))
